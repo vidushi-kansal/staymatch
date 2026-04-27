c981dcf06ab13ab08ebca7194f0dc81fe78678c1
vidushi-kansal <vidushikansal08@gmail.com>
2026-04-27 21:31:56 +0530

    Create chat.js

---
+++ backend/models/chat.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+    sender: String,
+    receiver: String,
+    message: String,
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Chat", chatSchema);
